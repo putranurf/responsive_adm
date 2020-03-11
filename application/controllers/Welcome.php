8b85bba95cbb7528ea0c60adfadf21cbd70fbbc2
Putra Nurfajar <putra@pindad.com>
2020-03-11 13:36:30 +0700

    Nambah dist popper

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,12 +18,12 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	// public function index()
-	// {
-	// 	$this->load->view('welcome_message');
-	// }
-
 	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function login()
 	{
 		$this->load->view('vlogin');
 	}
